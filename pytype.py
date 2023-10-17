17ed3d019e35265607bfbffdf1a88cb267ad7f31
GUCHITARO-S <143201098+GUCHITARO-S@users.noreply.github.com>
2023-10-17 16:03:56 +0900

    Update pytype.py

--- pytype.py
+++ pytype.py
@@ -18,26 +18,29 @@ def type_index():
     today = f"{day.month}月{day.day}日 "
     today_time = day.strftime("%H:%M")
     layout = [
-            [sg.Text(today+today_time,
-                     font=font, size=(20, 1),
-                     justification="center")],
-            [sg.Text("        "),
-             sg.Button("スタート", key="-START-", focus=True, bind_return_key=True),
-             sg.Button("おわり", key="-END-", bind_return_key=True)],
-        ]
+        [sg.Text(today+today_time,
+                 font=font, size=(20, 1),
+                 justification="center")],
+        [sg.Text("        "),
+         sg.Button("スタート", key="-START-",
+                   focus=True, bind_return_key=True),
+         sg.Button("おわり", key="-END-", bind_return_key=True)],
+    ]
 
     window = sg.Window("タイピング", layout, size=(250, 100))
     while True:
         event, value = window.read()
         if event in [sg.WIN_CLOSED, "-END-"]:
             break
-        if flag == 1:
-            window.close()
-            main()
+
         if event == "-START-":
-            flag = 1
-            main()
-            window.close()
+            if flag:
+                window.close()
+                main()
+            else:
+                flag = 1
+                main()
+                window.close()
 
 
 def main():
@@ -47,7 +50,8 @@ def main():
     total_time = 0
     count = 0
     words = []
-    files = ["csvs/typing.csv", "csvs/typing1.csv", "csvs/typing2.csv", "csvs/anime.csv", "csvs/hard.csv"]
+    files = ["csvs/typing.csv", "csvs/typing1.csv",
+             "csvs/typing2.csv", "csvs/anime.csv", "csvs/hard.csv"]
     file = random.choice(files)
     sc_dir = os.path.dirname(os.path.abspath(__file__))
     csv_file_path = os.path.join(sc_dir, file)
@@ -79,12 +83,12 @@ def main():
                 break
 
             try:
-                if pop_word.rstrip() == value["-I1-"]:
+                if pop_word.strip() == value["-I1-"].strip():
                     count += 1
                     end = time.perf_counter()
                     total_time += end - start
                     ans_word += value["-I1-"] + "\n"
-                    ans_time += f"問{count} -> {end - start:.2f}秒\n"
+                    ans_time += f"問{count:>02} -> {end - start:.2f}秒\n"
                     pop_word = next(gen_words)
                     window["-I1-"].update("")
                     window["-T1-"].update(pop_word)
@@ -112,13 +116,13 @@ def main():
             else:
                 continue
         layout = [
-                [sg.Text(f"合計入力時間：{int(total_time // 60)}分"
-                         f"{int(total_time % 60)}秒 -> 問題数：{count}",
-                         font=("meiryo", 20), justification="center")],
-                [sg.Text(f"{ans_word}", s=(60, 20), font=font1),
-                 sg.Text(f"{ans_time}", s=(15, 20), font=font1)],
-                [sg.Button("トップに戻る", key="-B1-", bind_return_key=True)],
-            ]
+            [sg.Text(f"合計入力時間：{int(total_time // 60)}分"
+                     f"{int(total_time % 60)}秒 -> 問題数：{count}",
+                     font=("meiryo", 20), justification="center")],
+            [sg.Text(f"{ans_word}", s=(60, 20), font=font1),
+             sg.Text(f"{ans_time}", s=(15, 20), font=font1)],
+            [sg.Button("トップに戻る", key="-B1-", bind_return_key=True)],
+        ]
 
         window2 = sg.Window("result", layout, size=(800, 600),
                             return_keyboard_events=True)
